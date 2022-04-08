d3495d69674c0841758c161f0fb3a706cffb001f
Lucas Lopes <lucas.lopes@kron.digital>
2022-04-08 08:01:30 -0300

    update first-development

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ import postsRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postsRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postsRoutes);
 
 const CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
